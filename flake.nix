5aafc9659eb8fe72f4a2e4a34eb53f17a569805f
Ethan Perruzza <ethan.perruzza@prologin.org>
2025-12-27 22:10:24 +0100

    ci: add nix fmt check

--- flake.nix
+++ flake.nix
@@ -11,8 +11,14 @@
     };
   };
 
-  outputs = { self, futils, nixpkgs }:
-    futils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      futils,
+      nixpkgs,
+    }:
+    futils.lib.eachDefaultSystem (
+      system:
       let
         inherit (nixpkgs) lib;
         pkgs = import nixpkgs { inherit system; };
@@ -22,7 +28,18 @@
         packages = wallpapers;
         apps.listPkgs = {
           type = "app";
-          program = toString (pkgs.writeShellScript "list.sh" (lib.concatMapStringsSep "\n" (el: "echo '${el}'") (builtins.attrNames self.packages.${system})));
+          program = toString (
+            pkgs.writeShellScript "list.sh" (
+              lib.concatMapStringsSep "\n" (el: "echo '${el}'") (builtins.attrNames self.packages.${system})
+            )
+          );
+        };
+        formatter = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            pre-commit
+          ];
         };
       }
     );
